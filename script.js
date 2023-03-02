eee36ee39312c3f25e95d8e4557eb0897a2c09dd
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-03-02 01:41:47 -0500

    More trolls

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-window.location.href = "https://youtu.be/a3Z7zEc7AXQ"; 
+const msg = document.getElementById("msg");
+
+switch(Math.floor(Math.random() * 3)) {
+	case 0:
+		msg.innerHTML = "Rickrolled!";
+		window.location.href = "https://youtu.be/a3Z7zEc7AXQ"; 
+		break;
+	case 1:
+		msg.innerHTML = "Coconut malled!";
+		window.location.href = "https://youtu.be/cGw-8FrRT1E"; 
+		break;
+	default:
+		msg.innerHTML = "Trolled!"
+		window.location.href = "https://youtu.be/gkTb9GP9lVI?t=4";
+		break;
+}
