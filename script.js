1e522c8b649466e630550cfe9f5ed74602b13174
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-03-17 00:36:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const msg = document.getElementById("msg");
 
-switch(Math.floor(Math.random() * 3)) {
+switch(Math.floor(Math.random() * 4)) {
 	case 0:
 		msg.innerHTML = "Rickrolled!";
 		window.location.href = "https://youtu.be/a3Z7zEc7AXQ"; 
@@ -8,6 +8,10 @@ switch(Math.floor(Math.random() * 3)) {
 	case 1:
 		msg.innerHTML = "Coconut malled!";
 		window.location.href = "https://youtu.be/cGw-8FrRT1E"; 
+		break;
+	case 2:
+        msg.innerHTML = "IndiHomed!";
+		window.location.href = "https://youtu.be/_IneeB-UDFA?t=60"; 
 		break;
 	default:
 		msg.innerHTML = "Trolled!"
