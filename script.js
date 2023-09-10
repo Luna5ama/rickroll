96a4926e3a6bfe81d9463ec4493410cd277e8462
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-09-10 17:46:39 -0400

    Update script.js
    Replaced tabs with spaces

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const msg = document.getElementById("msg");
 
 switch(Math.floor(Math.random() * 4)) {
-	case 0:
-		msg.innerHTML = "Rickrolled!";
-		window.location.href = "https://youtu.be/a3Z7zEc7AXQ"; 
-		break;
-	case 1:
-		msg.innerHTML = "Coconut malled!";
-		window.location.href = "https://youtu.be/cGw-8FrRT1E"; 
-		break;
-	case 2:
+    case 0:
+        msg.innerHTML = "Rickrolled!";
+        window.location.href = "https://youtu.be/a3Z7zEc7AXQ"; 
+        break;
+    case 1:
+        msg.innerHTML = "Coconut malled!";
+        window.location.href = "https://youtu.be/cGw-8FrRT1E"; 
+        break;
+    case 2:
         msg.innerHTML = "IndiHomed!";
-		window.location.href = "https://youtu.be/_IneeB-UDFA?t=60"; 
-		break;
-	default:
-		msg.innerHTML = "Trolled!"
-		window.location.href = "https://youtu.be/gkTb9GP9lVI?t=4";
-		break;
+        window.location.href = "https://youtu.be/_IneeB-UDFA?t=60"; 
+        break;
+    default:
+        msg.innerHTML = "Trolled!"
+        window.location.href = "https://youtu.be/gkTb9GP9lVI?t=4";
+        break;
 }
