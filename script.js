ca8a5169a5a11715b3b48c8dc095999b80b5e898
Luna <62033805+Luna5ama@users.noreply.github.com>
2024-03-03 18:00:55 -0500

    Adjusted troll distruction

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const msg = document.getElementById("msg");
 
-switch(Math.floor(Math.random() * 8)) {
+switch(Math.floor(Math.random() * 20)) {
     case 0:
     case 1:
     case 2:
+    case 3:
+    case 4:
         msg.innerHTML = "Rickrolled!";
         window.location.href = "https://youtu.be/mx86-rTclzA"; 
         break;
-    case 3:
-    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
         msg.innerHTML = "Coconut malled!";
         window.location.href = "https://youtu.be/cGw-8FrRT1E"; 
         break;
-    case 5:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
         msg.innerHTML = "Trolled!"
         window.location.href = "https://youtu.be/gkTb9GP9lVI?t=4";
         break;
-    case 6:
+    case 13:
+    case 14:
         msg.innerHTML = "Pull up!"
         window.location.href = "https://youtu.be/oLsVrshvOaI?t=10";
         break;
